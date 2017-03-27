5a56585c8d73858743e46d3244b3257d37cefe13
Eduard Frank <eddy@scanbot.io>
2017-03-27 11:00:59 +0200

    added example for initializeSdk()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+declare var ScanbotSdk: any;
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,6 +19,21 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // Initialize Scanbot SDK:
+      this.initScanbotSdk();
     });
+  }
+
+  private initScanbotSdk() {
+    var options = { loggingEnabled: true, licenseKey: '' };
+    ScanbotSdk.initializeSdk(function(result) {
+        console.log(result);
+      },
+      function(error) {
+        console.log('Error from Scanbot SDK: ' + error);
+      },
+      options
+    );
   }
 }
