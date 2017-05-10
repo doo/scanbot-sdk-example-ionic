6337dbd233a60339e6305f39b7db2245a92c2b6e
Eduard Frank <eddy@scanbot.io>
2017-05-10 12:23:31 +0200

    Use sampleSize option in Camera UI

--- src/pages/sdk-ui/sdk-ui.ts
+++ src/pages/sdk-ui/sdk-ui.ts
@@ -17,7 +17,8 @@ export class SdkUiPage {
   public startCameraUi() {
     let options = {
       edgeColor: '#0000ff',
-      quality: 70
+      quality: 70,
+      sampleSize: 2 // change to 1 for full resolution images
     };
     ScanbotSdkUi.startCamera(
       (result) => {
