148a69189ff1fb70ed9cc56869e4958533b536ca
Eduard Frank <eddy@scanbot.io>
2018-07-05 13:43:43 +0200

    Update cordova-plugin-scanbot-sdk to release version 2.0.0

--- pull-sbsdk-temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-adb pull /storage/emulated/0/Android/data/io.scanbot.example.sdk.ionic2/cache/sbsdk-temp
-adb shell rm '/storage/emulated/0/Android/data/io.scanbot.example.sdk.ionic2/cache/sbsdk-temp/*'
