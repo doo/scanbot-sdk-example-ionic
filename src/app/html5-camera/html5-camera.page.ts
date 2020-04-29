c33c01561a62b5fb92ea36b26886c4c91efc7a62
Nikituh <aare.undo@gmail.com>
2020-04-29 13:58:17 +0200

    4.1.0-alpha3: proper html scanner + real-time detection ui

--- src/app/html5-camera/html5-camera.page.ts
+++ src/app/html5-camera/html5-camera.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {ScanbotHTMLCamera} from '../../../plugins/cordova-plugin-scanbot-sdk/camera';
 
 @Component({
@@ -8,16 +8,29 @@ import {ScanbotHTMLCamera} from '../../../plugins/cordova-plugin-scanbot-sdk/cam
 })
 export class Html5CameraPage implements OnInit {
 
-  constructor() { }
+  public barcodeFormat: string;
+  public barcodeText: string;
 
-  async ngOnInit() {
-
-    const container = document.getElementById('cameraContainer');
-    const camera = await ScanbotHTMLCamera.create(container);
-    
-    camera.startBarcodeDetector(result => {
-      console.log('Result:', result);
-    });
+  constructor(private _ngZone: NgZone) {
+    this.barcodeFormat = '-';
+    this.barcodeText = '-';
   }
 
+  async ngOnInit() {
+    const container = document.getElementById('container');
+    const camera = await ScanbotHTMLCamera.create(container);
+
+    camera.startBarcodeDetector(result => {
+      if (result.barcodes.length === 0) {
+        return;
+      }
+
+      const barcode = result.barcodes[0];
+      // Is ngZone supposed to be like the main thread?
+      this._ngZone.run(() => {
+        this.barcodeFormat = barcode.type;
+        this.barcodeText = barcode.text;
+      });
+    });
+  }
 }
