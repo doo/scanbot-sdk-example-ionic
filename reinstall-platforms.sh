0cab605dc08ece14560d365d3e89fdfabc62deec
Nikituh <aare.undo@gmail.com>
2021-08-02 12:15:20 +0200

    added timing for reinstall script

--- reinstall-platforms.sh
+++ reinstall-platforms.sh
@@ -4,6 +4,8 @@
 # Helper script to reinstall platforms
 #
 
+SECONDS=0
+
 # remove and reinstall the platforms:
 ionic cordova platform remove ios
 ionic cordova platform remove android
@@ -19,4 +21,4 @@ npm install
 ionic cordova platform add ios@6.2.0
 ionic cordova platform add android@9.0.0
 
-
+echo "Complete! Full reinstall took: " $SECONDS " seconds"
