2543bb7b983a16af23369933c94cf8e184dfff61
Marco <marco@scanbot.io>
2022-05-03 14:05:19 +0200

    Improved reinstall-platforms script

--- reinstall-platforms.sh
+++ reinstall-platforms.sh
@@ -4,19 +4,34 @@
 # Helper script to reinstall platforms
 #
 
-# remove and reinstall the platforms:
+DEFAULT_IOS_PLATFORM_VERSION=6.2.0
+DEFAULT_ANDROID_PLATFORM_VERSION=9.0.0
+
+read -p "iOS Platform Version (press ENTER for $DEFAULT_IOS_PLATFORM_VERSION): " ios_platform_version
+read -p "Android Platform Version (press ENTER for $DEFAULT_ANDROID_PLATFORM_VERSION): " android_platform_version
+
+if [ ! -n "$ios_platform_version" -a "$ios_platform_version" != " " ]; then
+    ios_platform_version=$DEFAULT_IOS_PLATFORM_VERSION
+fi
+
+if [ ! -n "$android_platform_version" -a "$android_platform_version" != " " ]; then
+    android_platform_version=$DEFAULT_ANDROID_PLATFORM_VERSION
+fi
+
+# Remove the platforms
 ionic cordova platform remove ios
 ionic cordova platform remove android
 
+# Clean the cache
 rm -rf platforms/
 rm -rf node_modules/
 rm -rf plugins/
 npm cache clean --force
 rm package-lock.json
 
+# Reinstall node modules
 npm install
 
-ionic cordova platform add ios@6.2.0
-ionic cordova platform add android@9.0.0
-
-
+# Reinstall the platforms
+ionic cordova platform add ios@$ios_platform_version
+ionic cordova platform add android@$android_platform_version
